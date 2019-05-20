1f89f4ddc94734d20784c7f64c69362e759d4a13
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2019-05-20 10:35:05 +0200

    Black dick

--- investiga.py
+++ investiga.py
@@ -4,7 +4,7 @@ import búsqueda_espacio_estados as busqee
 
 import copy
 
-estado1 = [{1,2,1},{2,2,1},{3,1,2}]
+estado1 = [{1,2,1},{2,2,1},{1,1,2}]
 
 class CrossOut(probee.Acción):
     def __init__(self,i,j):
@@ -21,17 +21,46 @@ class CrossOut(probee.Acción):
         return  bool(estado[self.cell_row].count(estado[self.cell_row][self.cell_colum]) > 1)
 
     def exist_in_colum(self, estado):
-        return bool(estado[self.cell_colum].count(estado[self.cell_row][self.cell_colum]) > 1)
+        res = 0
+        for i in range(len(estado)):
+            if estado[i][self.cell_colum] == estado[self.cell_row][self.cell_colum]:
+                res = res + 1
+        aux = False
+        if res > 1:
+            aux = True
+        return aux
+
+    def exist_black_cell_around(self, estado):
+        aux = 0
+        actual = estado[self.cell_row][self.cell_colum] == 0
+        if self.cell_row != 0 and self.cell_row != 0 and estado[self.cell_row-1][self.cell_colum] == 0:
+            aux = aux + 1
+        if self.cell_colum != 0 and estado[self.cell_row][self.cell_colum-1] == 0:
+            aux = aux + 1
+        if self.cell_colum != len(estado[self.cell_row])-1 and estado[self.cell_row][self.cell_colum+1] == 0:
+            aux = aux + 1
+        if self.cell_row != len(estado)-1 and estado[self.cell_row+1][self.cell_colum] == 0:
+            aux = aux + 1
+        if actual and aux > 0:
+            return True
+        if aux == 4:
+            return True
+        if aux > 1 and (self.cell_row == 0 or self.cell_row == len(estado[self.cell_row])):
+            return True
+        if aux > 1 and (self.cell_colum == 0 or self.cell_colum == len(estado)):
+            return True
+        return False
 
 
 
 if __name__ == '__main__':
     print("hola")
-    estado1 = [[1,2,1],[2,2,1],[3,1,2]]
-    print(estado1[0][0])
+    estado1 = [[0,0,1],[1,1,1],[1,1,1]]
+    print(estado1[1][1])
     pom = CrossOut(0,0)
     print(pom.exist_in_row(estado1))
     print(pom.exist_in_colum(estado1))
+    print(pom.exist_black_cell_around(estado1))
 
 
 
