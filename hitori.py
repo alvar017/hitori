f881539bb5f2a1dd56c10e9381e814114d035b62
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2019-06-13 12:59:09 +0200

    Traspuesta

--- hitori.py
+++ hitori.py
@@ -10,20 +10,27 @@ class hitori(probee.ProblemaEspacioEstados):
     def __init__(self, estado):
         super().__init__(estado)
         self.estado = estado
+        estado_traspuesta = [[self.estado[j][i] for j in range(len(self.estado))] for i in range(len(self.estado[0]))]
+        self.estado_traspuesta = estado_traspuesta
+        print("Normal")
+        print(estado[0])
+        print(estado[1])
+        print(estado[2])
+        print(estado[3])
+        print("Normal")
+        print("Traspuesta")
+        print(self.estado_traspuesta[0])
+        print(self.estado_traspuesta[1])
+        print(self.estado_traspuesta[2])
+        print(self.estado_traspuesta[3])
+        print("Traspuesta")
 
-    def es_estado_final(self, estado):
+    def es_estado_final(self):
         filas = True
         columnas = True
-        print(estado)
-        for i in range(len(estado)):
-            to_check_row = list(filter(lambda x: x!=0, estado[i]))
+        for i in range(len(self.estado)):
+            to_check_row = list(filter(lambda x: x!=0, self.estado[i]))
             to_check_row_distinct = list(set(to_check_row))
-            if(len(to_check_row) != len(to_check_row_distinct)):
-               filas = False
-               break
-        for i in range(len(estado[0])):
-            to_check_colum = list(filter(lambda x: x!=0, estado[i]))
-            to_check_colum_distinct = list(set(to_check_row))
             if(len(to_check_row) != len(to_check_row_distinct)):
                filas = False
                break
@@ -32,5 +39,5 @@ class hitori(probee.ProblemaEspacioEstados):
 if __name__ == '__main__':
     estado = [[1, 2, 3, 4], [1, 2, 3, 4], [2, 5, 6, 7], [3, 0, 6, 2]]
     pom = hitori(estado)
-    print(pom.es_estado_final(pom.estado))
+    print(pom.es_estado_final())
 
