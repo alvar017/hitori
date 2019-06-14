4a781934d3cfab0eeb650f9dd7148d8efa8d5a77
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2019-06-14 16:49:21 +0200

    Reto en 1 segundo
    No se si funciona bien del todo

--- hitori.py
+++ hitori.py
@@ -4,7 +4,6 @@ import búsqueda_espacio_estados as busqee
 
 import investiga as investiga
 
-import copy
 
 class hitori(probee.ProblemaEspacioEstados):
     def __init__(self, estado):
@@ -12,31 +11,24 @@ class hitori(probee.ProblemaEspacioEstados):
         self.estado = estado
         acciones = [investiga.CrossOut(i, j) for i in range(0, len(estado)) for j in range(0, len(estado[0]))]
         self.estado_inicial = self.estado
-
-        estado_traspuesta = [[self.estado[j][i] for j in range(len(self.estado))] for i in range(len(self.estado[0]))]
-        self.estado_traspuesta = estado_traspuesta
+        self.estado_traspuesta = [[self.estado[j][i] for j in range(len(self.estado))] for i in range(len(self.estado[0]))]
 
         super().__init__(acciones, self.estado_inicial)
 
-    def aplicables(self):
-        acciones = []
-        x = 0
-        for i in range(len(self.estado)):
-            for j in range(len(self.estado[0])):
-                if (self.estado[i][j] != 0):
-                    copy_estado = copy.deepcopy(self.estado)
-                    copy_estado[i][j] = 0
-                    acciones.append(copy_estado)
-                    x = x + 1
-        return acciones
-
 if __name__ == '__main__':
-    estado = [[3,1,3,4],[1,2,4,3],[3,1,1,4],[3,4,2,4]]
+    estado = [[3,4,5,5,1,3],[5,6,2,3,2,1],[5,3,1,4,5,4],[1,4,3,4,2,2],[3,1,6,1,4,5],[1,2,1,5,3,4]]
     pom = hitori(estado)
+    hitori_resolver = probee.ProblemaEspacioEstados(pom.acciones, pom.estado_inicial)
 
-    b_anchura = busqee.BúsquedaEnAnchura(detallado=True)
-    acciones = [investiga.CrossOut(i, j) for i in range(0, len(pom.estado)) for j in range(0, len(pom.estado[0]))]
-    hitori_resolver = probee.ProblemaEspacioEstados(acciones, pom.estado_inicial)
-    print(b_anchura.buscar(hitori_resolver))
+    # Búsqueda en anchura
+
+#    b_anchura = busqee.BúsquedaEnAnchura(detallado=True)
+#    print(b_anchura.buscar(hitori_resolver))
+
+    # Búsqueda en profundidad
+
+    b_profundidad = busqee.BúsquedaEnProfundidad(detallado=True)
+    print(b_profundidad.buscar(hitori_resolver))
+
 
 
